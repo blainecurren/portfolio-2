7908c58dcb057d667fa20d1b87634da413a94021
Blaine Curren <blainecurren@Blaines-MacBook-Pro.local>
2022-09-02 20:02:16 -0500

    initial intro css styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
-function App() {
+import React from "react";
+import Intro from "./components/intro/Intro";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Intro />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,1 +1,27 @@
-q
+import React from "react";
+import "./intro.css";
+
+const Intro = () => {
+  return (
+    <div className="i">
+      <div className="i-left">
+        <div className="i-left-wrapper">
+          <h2 className="i-intro">Hello, my name is</h2>
+          <h1 className="i-name">Blaine Curren</h1>
+          <div className="i-title">
+            <div className="i-title-wrapper">
+              <div className="i-title-item">Front-end Web Developer</div>
+              <div className="i-title-item">Back-end Web Developer</div>
+              <div className="i-title-item">Photographer</div>
+              <div className="i-title-item">Writer</div>
+              <div className="i-title-item">Content Creator</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="i-right">right</div>
+    </div>
+  );
+};
+
+export default Intro;
