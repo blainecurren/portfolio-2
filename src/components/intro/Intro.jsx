d9da24cb9f5764963f0daa3b7175fb42fee377fe
Blaine Curren <blainecurren@Blaines-MacBook-Pro.local>
2022-09-02 22:49:23 -0500

    finished intro component

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./intro.css";
+// import Me from "../../img/me.png";
 
 const Intro = () => {
   return (
@@ -10,16 +11,24 @@ const Intro = () => {
           <h1 className="i-name">Blaine Curren</h1>
           <div className="i-title">
             <div className="i-title-wrapper">
-              <div className="i-title-item">Front-end Web Developer</div>
-              <div className="i-title-item">Back-end Web Developer</div>
+              <div className="i-title-item">Front-End Web Developer</div>
+              <div className="i-title-item">Back-End Web Developer</div>
               <div className="i-title-item">Photographer</div>
               <div className="i-title-item">Writer</div>
               <div className="i-title-item">Content Creator</div>
             </div>
           </div>
+          <p className="i-desc">
+            I design and develop services for customers of all sizes,
+            specializing in creating stylish, modern websites, web services, and
+            online stores.
+          </p>
         </div>
       </div>
-      <div className="i-right">right</div>
+      <div className="i-right">
+        <div className="i-bg"></div>
+        {/* <img src={Me} alt="" className="i-img" /> */}
+      </div>
     </div>
   );
 };
