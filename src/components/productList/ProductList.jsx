c7b083f94f59cd8fc302bc9f6e17109cba105e48
Blaine Curren <blainecurren@Blaines-MacBook-Pro.local>
2022-09-03 12:50:15 -0500

    finished cards and props for links and images

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./productList.css";
 import Product from "../product/Product";
+import { products } from "../../data.js";
 
 const ProductList = () => {
   return (
@@ -14,11 +15,9 @@ const ProductList = () => {
         </p>
       </div>
       <div className="pl-list">
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        {products.map((item) => (
+          <Product key={item.id} img={item.img} link={item.link} />
+        ))}
       </div>
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/4TD6cYF/1.jpg",
+    link: "https://github.com/blainecurren",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/4TD6cYF/2.jpg",
+    link: "https://github.com/blainecurren",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/4TD6cYF/3.jpg",
+    link: "https://github.com/blainecurren",
+  },
+  {
+    id: 4,
+    img: "https://i.ibb.co/4TD6cYF/4.jpg",
+    link: "https://github.com/blainecurren",
+  },
+  {
+    id: 5,
+    img: "https://i.ibb.co/4TD6cYF/5.jpg",
+    link: "https://github.com/blainecurren",
+  },
+  {
+    id: 6,
+    img: "https://i.ibb.co/4TD6cYF/6.jpg",
+    link: "https://github.com/blainecurren",
+  },
+];
