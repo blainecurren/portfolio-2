3dc23bb00ee93cef0ebb0e63256142e50847c4f8
Blaine Curren <blainecurren@Blaines-MacBook-Pro.local>
2022-09-06 08:21:36 -0500

    added topbar and menu functionality / still need to push hamburger span to the right

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,18 @@ import About from "./components/about/About";
 import ProductList from "./components/productList/ProductList";
 import Contact from "./components/contact/Contact";
 import "./app.scss";
+import { useState } from "react";
+import Topbar from "./components/topbar/Topbar";
+import Menu from "./components/menu/Menu";
 
 const App = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
-      <Intro />
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
+        <Intro />
         <About />
         <ProductList />
         <Contact />
