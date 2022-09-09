c3a014f90dd6a0f3d22996d21d4cc47d84596351
Blaine Curren <blainecurren@Blaines-MacBook-Pro.local>
2022-09-09 07:13:38 -0500

    added top / right prop to intro memoji

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -7,7 +7,7 @@ const Intro = () => {
     <div className="i">
       <div className="i-left">
         <div className="i-left-wrapper">
-          <h2 className="i-intro">Hi there, my name is,</h2>
+          <h2 className="i-intro">Hi there, I'm</h2>
           <h1 className="i-name">Blaine.</h1>
           <div className="i-title">
             <div className="i-title-wrapper">
