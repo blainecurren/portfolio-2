fec82b2b1afe176e95dadc6328cb3529bf07dbee
Blaine Curren <blaine.curren@gmail.com>
2022-09-17 21:27:19 -0500

    fixed memoji in about me

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -8,11 +8,6 @@ const ProductList = () => {
     <div className="pl">
       <div className="pl-texts">
         <h1 className="pl-title">Take a look at a couple of my projects!</h1>
-        <p className="pl-desc">
-          This is a creative portfolio that I've been working on the past year
-          and a half. Front end to back end, this portfolio has a little bit of
-          it all.
-        </p>
       </div>
       <div className="pl-list">
         {products.map((item) => (
