dbada46c5a243a84790124b07761f549d1dbf4ac
Blaine Curren <blaine.curren@gmail.com>
2022-09-20 11:09:41 -0500

    commit before changing branches

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -8,7 +8,7 @@ export default function Menu({ menuOpen, setMenuOpen }) {
           <a href="#intro">Home</a>
         </li>
         <li onClick={() => setMenuOpen(false)}>
-          <a href="#aboutme">About Me</a>
+          <a href="#about">About Me</a>
         </li>
         <li onClick={() => setMenuOpen(false)}>
           <a href="#productList">Portfolio</a>
