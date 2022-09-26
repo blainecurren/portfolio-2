ad57218b5ffcb5281b69d1f79ee7dfdd047286ff
Blaine Curren <blaine.curren@gmail.com>
2022-09-26 12:36:50 -0500

    added images and links work properly -- need to add images dynamically and establish links

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./product.css";
+import screenshot from "../../img/screenshot-test.png";
 
 const Product = ({ img, link }) => {
   return (
@@ -10,7 +11,7 @@ const Product = ({ img, link }) => {
         <div className="p-circle"></div>
       </div>
       <a href={link} target="_blank" rel="noreferrer">
-        <img src={img} alt="" className="p-img" />
+        <img src={screenshot} alt="" className="p-img" />
       </a>
     </div>
   );
