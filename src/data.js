4e0df806e30a3d664806fa3353a77b120a7a986d
Blaine Curren <blaine.curren@gmail.com>
2022-09-30 15:07:44 -0500

    added project names

--- src/data.js
+++ src/data.js
@@ -1,35 +1,38 @@
 import photo from "../src/img/screenshot-test.png";
 
-export const photosArr = [photo];
-
 export const products = [
   {
     id: 1,
-    img: "{ photo }",
+    title: "u-develop-it",
+    img: photo,
     link: "https://github.com/blainecurren",
   },
   {
     id: 2,
+    title: "pizza-hunt",
     img: "",
     link: "https://github.com/blainecurren",
   },
   {
     id: 3,
+    title: "API's",
     img: "",
     link: "https://github.com/blainecurren",
   },
   {
     id: 4,
+    title: "deep-thoughts",
     img: "",
     link: "https://github.com/blainecurren",
   },
   {
     id: 5,
-    img: "",
+    img: "shop-shop",
     link: "https://github.com/blainecurren",
   },
   {
     id: 6,
+    title: "",
     img: "",
     link: "https://github.com/blainecurren",
   },
