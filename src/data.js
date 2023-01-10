71df725460d5f2b21042d94618b80dffcf2d2475
Blaine Curren <blaine.curren@gmail.com>
2023-01-10 07:36:01 -0600

    imported all project photos

--- src/data.js
+++ src/data.js
@@ -1,5 +1,9 @@
 import robo from "../src/img/robot-glad.jpg";
 import buddy from "../src/img/run-buddy.png";
+import pizza from "../src/img/pizza-hunt.png";
+import shop from "../src/img/shop-shop.png";
+import thoughts from "../src/img/deep-thoughts.png";
+import develop from "../src/img/u-develop-it.png";
 
 export const projects = [
   {
@@ -16,25 +20,26 @@ export const projects = [
   },
   {
     id: 3,
-    title: "API's",
-    img: "",
+    title: "u-develop-it",
+    img: develop,
     link: "https://github.com/blainecurren",
   },
   {
     id: 4,
-    title: "deep-thoughts",
-    img: "",
+    title: "pizza-hunt",
+    img: pizza,
     link: "https://github.com/blainecurren",
   },
   {
     id: 5,
-    img: "shop-shop",
+    title: "deep-thoughts",
+    img: thoughts,
     link: "https://github.com/blainecurren",
   },
   {
     id: 6,
-    title: "",
-    img: "",
+    title: "shop-shop",
+    img: shop,
     link: "https://github.com/blainecurren",
   },
 ];
